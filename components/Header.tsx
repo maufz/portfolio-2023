e3b686beaadc657b3e445d1d6e22b3d034ea663e
Mauro Fernández <100251438+maufz@users.noreply.github.com>
2023-05-06 00:09:26 -0300

    Header profile img loading fix

--- components/Header.tsx
+++ components/Header.tsx
@@ -12,7 +12,7 @@ export default function Header() {
     <div className={`relative ${coverState} bg-gradient-to-b from-teal-400 to-cyan-600 after:absolute after:inset-0 after:backdrop-blur after:border-b -after:border-black after:border-opacity-10 transition-all`} onClick={handleCoverClick}></div>
     <div className="relative border-b border-black border-opacity-10 border-dashed">
       <div className="max-w-2xl mx-auto px-3 flex items-center">
-        <img src="profile.webp" className="w-auto h-[120px] rounded-full mt-[-60px] inline-block border-2 border-white mr-2" alt="" />
+        <img src="profile.webp" className="w-[120px] h-[120px] rounded-full mt-[-60px] inline-block border-2 border-white mr-2" alt="" />
         <div className="flex items-center gap-[2px]">
           <a href="https://github.com/maufz" className="p-2 hover:opacity-80 transition-opacity" target="_blank"><img src="mdi_github.svg" alt="" /></a>
           <a href="https://www.linkedin.com/in/mauro-fern%C3%A1ndez-6a3052198/" className="p-2 hover:opacity-80 transition-opacity" target="_blank"><img src="akar-icons_linkedin.svg" alt="" /></a>
