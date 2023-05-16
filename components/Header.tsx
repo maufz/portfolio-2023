5936f77c030436f6896a9b09fb3c07f0260031e0
Mauro Fernández <100251438+maufz@users.noreply.github.com>
2023-05-16 19:40:12 -0300

    primeras animaciones

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react"
-import dottedBg from "@/public/dotted-bg.png";
+import dottedBg from "@/public/dotted-bg.png"
+import { motion } from "framer-motion";
 
 export default function Header() {
   const states = ['h-24 lg:h-44', 'h-96'];
@@ -10,7 +11,11 @@ export default function Header() {
   }
 
   return <header>
-    <div style={{backgroundImage: `url(${dottedBg.src})`}} className={`${coverState} transition-all bg-teal-400`} onClick={handleCoverClick}></div>
+    <div style={{backgroundImage: `url(${dottedBg.src})`}} className={`${coverState} transition-all bg-teal-400 relative`} onClick={handleCoverClick}>
+      <motion.div className="absolute top-0 left-0 w-full bg-[#F1F1F1]" initial={{height: '100%'}} animate={{height: 0}} transition={{duration: 0.5, delay: 0.2, type: 'spring'}}>
+
+      </motion.div>
+    </div>
     <div className="relative border-b border-black border-opacity-10 border-dashed">
       <div className="max-w-2xl mx-auto px-3 flex items-center">
         <img src="profile.webp" className="w-[120px] h-[120px] rounded-full mt-[-60px] inline-block border-2 border-white mr-2" alt="" />
