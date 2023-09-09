fcf254c5a3da5523e47bd6a5391a179ce39a84f4
Mauro Fernández <100251438+maufz@users.noreply.github.com>
2023-09-09 19:58:41 -0300

    Probando cosas

--- components/Portfolios.tsx
+++ components/Portfolios.tsx
@@ -1,5 +1,50 @@
 import { motion } from "framer-motion";
 import { container, item } from "../utils/nested-animations";
+import {FiInfo} from "react-icons/fi";
+
+const portfolios = [
+  {
+    title: "Peregrinus",
+    img: "portfolios/peregrinus2.webp",
+    icon: "portfolios/peregrinus-icon.webp",
+    link: "https://peregrinus.com.uy",
+  },
+  {
+    title: "Midinero",
+    img: "portfolios/midinero2.webp",
+    icon: "portfolios/midinero-icon.webp",
+    link: "https://midinero.com.uy",
+    imgWrapperClasses: "bg-[#C2E2FF]",
+  },
+  {
+    title: "Innuy",
+    img: "portfolios/innuy2.webp",
+    icon: "portfolios/innuy-icon.webp",
+    link: "https://innuy.com",
+    imgWrapperClasses: "bg-[#FBD4B6]",
+  },
+  {
+    title: "Seva.Love",
+    img: "portfolios/seva-love.webp",
+    icon: "portfolios/seva-love-icon.webp",
+    link: "https://seva.love",
+    imgWrapperClasses: "bg-[#D2C3EF]",
+  },
+  {
+    title: "Edward & Associates",
+    img: "portfolios/edwards-associates.webp",
+    icon: "portfolios/edwards-associates-icon.svg",
+    link: "https://eandassociates.com/",
+    imgWrapperClasses: "bg-[#D0F1C0]",
+  },
+  {
+    title: "Market Street Family Dental",
+    img: "portfolios/market-street-dental.webp",
+    icon: "portfolios/market-street-dental-icon.webp",
+    link: "https://troyildentist.com/",
+    imgWrapperClasses: "bg-[#C0F1DA]",
+  },
+]
 
 export default function Portfolios() {
   return (
@@ -11,119 +56,48 @@ export default function Portfolios() {
         animate="visible"
         viewport={{ once: true }}
       >
-        <PortfolioItem
-          title="Peregrinus"
-          img="portfolios/peregrinus2.webp"
-          icon="portfolios/peregrinus-icon.webp"
-          link="https://peregrinus.com.uy"
-        />
-        <PortfolioItem
-          title="Midinero"
-          img="portfolios/midinero2.webp"
-          icon="portfolios/midinero-icon.webp"
-          link="https://midinero.com.uy"
-          imgWrapperClasses="bg-[#C2E2FF]"
-        />
-        <PortfolioItem
-          title="Innuy"
-          img="portfolios/innuy2.webp"
-          icon="portfolios/innuy-icon.webp"
-          link="https://innuy.com"
-          imgWrapperClasses="bg-[#FBD4B6]"
-        />
-        <PortfolioItem
-          title="Seva.Love"
-          img="portfolios/seva-love.webp"
-          icon="portfolios/seva-love-icon.webp"
-          link="https://seva.love"
-          imgWrapperClasses="bg-[#D2C3EF]"
-        />
-        <PortfolioItem
-          title="Edward & Associates"
-          img="portfolios/edwards-associates.webp"
-          icon="portfolios/edwards-associates-icon.svg"
-          link="https://eandassociates.com/"
-          imgWrapperClasses="bg-[#D0F1C0]"
-        />
-        <PortfolioItem
-          title="Market Street Family Dental"
-          img="portfolios/market-street-dental.webp"
-          icon="portfolios/market-street-dental-icon.webp"
-          link="https://troyildentist.com/"
-          imgWrapperClasses="bg-[#C0F1DA]"
-        />
+        {portfolios.map(portfolio => <PortfolioItem
+          portfolio={portfolio}
+          key={portfolio.title}
+        />)}
       </motion.div>
     </section>
   );
 }
 
 interface ItemProps {
-  title: string;
-  img: string;
-  icon?: string;
-  link: string;
-  variant?: "variant1" | "variant2" | "variant3";
-  imgWrapperClasses?: string;
+  onOpenModal?: (event: any ) => void;
+  portfolio: {
+    title: string;
+    img: string;
+    icon?: string;
+    link: string;
+    variant?: "variant1" | "variant2" | "variant3";
+    imgWrapperClasses?: string;
+  }
 }
 
-function PortfolioItem(props: ItemProps) {
-  const { title, img, icon, link, variant = "variant3" } = props;
-
-  if (variant == "variant2") return <PortfolioItemVariant2 {...props} />;
-  if (variant == "variant3") return <PortfolioItemVariant3 {...props} />;
-
-  return (
-    <motion.a href={link} target="_blank" variants={item}>
-      <article className="p-6 border border-black border-opacity-20 rounded-2xl bg-white bg-opacity-50 flex flex-col gap-4 h-full hover:shadow transition-shadow">
-        <img
-          src={img}
-          alt={`Captura de pantalla de la página de inicio de ${title}`}
-          className={`aspect-[3/2] w-full object-cover rounded-lg border border-black border-opacity-10 bg-white`}
-        />
-        <div className="flex items-center gap-3">
-          {icon && (
-            <img
-              src={icon}
-              className="rounded-lg border border-black border-opacity-10 w-8 h-8 object-cover"
-            />
-          )}
-          <h1 className="text-xl font-medium font-headings">{title}</h1>
-        </div>
-      </article>
-    </motion.a>
-  );
-}
-
-function PortfolioItemVariant2({ title, img, icon, link }: ItemProps) {
-  return (
-    <a href={link} target="_blank">
-      <article className="border border-black border-opacity-20 rounded-2xl bg-white bg-opacity-50 flex flex-col gap-4 h-full hover:shadow transition-shadow">
-        <img
-          src={img}
-          alt=""
-          className="aspect-[3/2] w-full object-cover rounded-t-2xl border border-black border-opacity-10 bg-white"
-        />
-        <div className="flex items-center gap-3 px-4 pb-5">
-          {icon && (
-            <img
-              src={icon}
-              className="rounded-lg border border-black border-opacity-10 w-8 h-8 object-cover"
-            />
-          )}
-          <h1 className="text-xl font-medium font-headings">{title}</h1>
-        </div>
-      </article>
-    </a>
-  );
-}
-
-function PortfolioItemVariant3({
-  title,
-  img,
-  icon,
-  link,
-  imgWrapperClasses = "bg-[#CFE5E7]",
+function PortfolioItem({
+  onOpenModal,
+  portfolio
 }: ItemProps) {
+
+  const {title,
+    img,
+    icon,
+    link,
+    imgWrapperClasses = "bg-[#CFE5E7]"} = portfolio;
+
+  const openInfoModal = (event: any) => {
+    event.preventDefault();
+    onOpenModal({
+      title,
+      img,
+      icon,
+      link,
+    });
+  }
+
   return (
     <motion.a
       href={link}
@@ -141,17 +115,20 @@ function PortfolioItemVariant3({
             className="aspect-video w-full object-cover rounded-lg bg-white mb-[2] group-hover:-translate-y-1 transition-transform group-hover:duration-300"
           />
         </div>
-        <div className="flex items-center gap-2 md:gap-3 p-4 xl:px-6 xl:pb-6 xl:pt-5">
-          {icon && (
-            <img
-              src={icon}
-              alt={`Logo de ${title}`}
-              className="rounded-lg border border-black border-opacity-10 w-8 h-8 object-cover"
-            />
-          )}
-          <h1 className="text-md lg:text-xl font-medium font-headings">
-            {title}
-          </h1>
+        <div className="flex items-center justify-between p-4 xl:px-6 xl:pb-6 xl:pt-5">
+          <div className="flex items-center gap-2 md:gap-3">
+            {icon && (
+              <img
+                src={icon}
+                alt={`Logo de ${title}`}
+                className="rounded-lg border border-black border-opacity-10 w-8 h-8 object-cover"
+              />
+            )}
+            <h1 className="text-md lg:text-xl font-medium font-headings">
+              {title}
+            </h1>
+          </div>
+          <FiInfo className="opacity-0 group-hover:opacity-100 transition-all hover:bg-neutral-200 rounded-full w-8 h-8 p-2 shrink-0" onClick={openInfoModal} />
         </div>
       </article>
     </motion.a>
